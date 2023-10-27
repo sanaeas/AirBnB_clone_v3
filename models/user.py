9db4036ab0f2f9f293bce693b11405edbc0a20b4
Sanae <sanaassim1@gmail.com>
2023-10-27 07:42:26 -0700

    Security improvements

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, key, val):
+        """sets a hashed password to a MD5 value"""
+        if key == "password":
+            val = hashlib.md5(val.encode()).hexdigest()
+        super().__setattr__(key, val)
