0d058aa3371271325d683ff5065ff134c70cac6c
Sanae <sanaassim1@gmail.com>
2023-10-27 04:19:05 -0700

    Fix error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -24,7 +24,8 @@ def get_state(state_id):
         abort(404)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """deletes a state by id"""
     state = storage.get(State, state_id)
